48f8f7362e295341c6c6511d4299c0640e86c6c8
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2022-02-25 05:37:12 +0100

    Utilise le mapping au lieu des tableaux dynamiques

--- contracts/Vock.sol
+++ contracts/Vock.sol
@@ -1,44 +1,39 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.7.0 <0.9.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
 
+/** 
+ * @title Vock
+ * @dev Implémente le mecanisme de vote
+ */
 contract Vock {
 
-    struct Vote {
-        address electeur;
-        uint candidat;
-        uint election;
-    }
+    // nbreVotes : pour stocker les votes pour un candidat donné 
+    mapping(uint => uint) public nbreVotes;
 
-    Vote[] private votes;
+    // totalVotes : pour stocker le nombre de votes pour une élection donnée
+    mapping(uint => uint) public totalVotes;
 
+    // votes : pour stocker les votes des électeurs
+    mapping(uint => mapping(address => uint)) private votes;
+
+    /**
+     * @dev Enregistre le vote d'un électeur.
+     * @param _election l'identifiant de l'élection où se trouve le candidat
+     * @param _candidat l'identifiant du candat dans l'élection
+     */
     function voter(uint _election, uint _candidat) public {
         require(getVote(_election) == 0, "Ce compte a deja vote");
-        votes.push(Vote({
-            electeur: msg.sender,
-            candidat: _candidat,
-            election: _election
-        }));
+        votes[_election][msg.sender] = _candidat;
+        nbreVotes[_candidat] += 1;
+        totalVotes[_election] += 1;
     }
 
-    function totalVotes(uint _election) public view returns (uint nbre) {
-        for (uint i = 0; i < votes.length; i++) {
-            if (votes[i].election == _election) 
-                nbre++;
-        }
-    }
-
+    /** 
+     * @dev Récupérer le vote d'un électeur en fournissant l'identifiant de l'élection
+     * @param _election l'identifiant de l'élection
+     * @return l'identifiant du candidat voté
+     */
     function getVote(uint _election) public view returns (uint) {
-        for (uint i = 0; i < votes.length; i++) {
-            if (votes[i].election == _election && votes[i].electeur == msg.sender) 
-                return votes[i].candidat;
-        }
-        return 0;
-    }
-
-    function nbreVotes(uint _election, uint _candidat) public view returns (uint nbre) {
-        for (uint i = 0; i < votes.length; i++) {
-            if (votes[i].election == _election && votes[i].candidat == _candidat) 
-                nbre++;
-        }
+        return votes[_election][msg.sender];
     }
 }
