1c06a555a04894c9a1533dda5ed80b99aaf2b86f
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2022-02-25 22:53:15 +0100

    Crée les tests

--- contracts/Vock.sol
+++ contracts/Vock.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.12;
+pragma solidity ^0.8.11;
 
 /** 
  * @title Vock
@@ -7,33 +7,94 @@ pragma solidity ^0.8.12;
  */
 contract Vock {
 
-    // nbreVotes : pour stocker les votes pour un candidat donné 
-    mapping(uint => uint) public nbreVotes;
+    // Différents statuts d'une élection
+    enum Statut {
+        Initial,  // L'élection est consiérée comme inexistante
+        Cree,     // Quand quelqu'un crée une élection. Enregistrement des candidat
+        EnCours,  // Lorsque l'élection est lancée. Il n'est plus possible d'enregistrer des candidats
+        Termine   // Lorsque l'émection est terminée. Plus personne ne peut voter.
+    }
 
-    // totalVotes : pour stocker le nombre de votes pour une élection donnée
-    mapping(uint => uint) public totalVotes;
+    // La structure représentant une élection
+    struct Election {
+        uint totalVotes;                     // Nombre total de vote pour cette élection
+        Statut statut;                       // Statut de l'élection. Par défaut c'est Initial
+        address auteur;                      // Celui qui a créé l'élection
+        mapping(address => uint) votes;      // Tous les votes pour cette élection
+        mapping(uint => Candidat) candidats; // Liste des candidats pour cette élection
+    }
 
-    // votes : pour stocker les votes des électeurs
-    mapping(uint => mapping(address => uint)) private votes;
+    // La structure représentant un candidat
+    struct Candidat {
+        string nom;      // Le nom du candidat
+        uint nbreVotes;  // Le nombre de personnes ayant votés pour lui
+        bool enregistre; // Pour savoir s'il est enregistré pour l'élection
+    }
+
+    // Un modifier pour vérifier si l'élection est en cours
+    modifier electionEnCours(uint _idElec) {
+        require(elections[_idElec].statut == Statut.EnCours, "Cette election n'est pas en cours");
+        _;
+    }
+
+    // Un modifier pour vérifier si l'adresse correspond à celle de l'auteur de l'élection
+    modifier auteurSeulement(uint _idElec) {
+        require(elections[_idElec].auteur == msg.sender, "Vous n'etes pas l'auteur de cette election");
+        _;
+    }
+
+    // elections : pour stocker les élections
+    mapping(uint => Election) private elections;
 
     /**
      * @dev Enregistre le vote d'un électeur.
-     * @param _election l'identifiant de l'élection où se trouve le candidat
-     * @param _candidat l'identifiant du candat dans l'élection
+     * @param _idElec l'identifiant de l'élection où se trouve le candidat
+     * @param _idCand l'identifiant du candidat dans l'élection
      */
-    function voter(uint _election, uint _candidat) public {
-        require(getVote(_election) == 0, "Ce compte a deja vote");
-        votes[_election][msg.sender] = _candidat;
-        nbreVotes[_candidat] += 1;
-        totalVotes[_election] += 1;
+    function voter(uint _idElec, uint _idCand) electionEnCours(_idElec) public {
+        require(elections[_idElec].votes[msg.sender] == 0, "Ce compte a deja vote");
+        require(elections[_idElec].candidats[_idCand].enregistre == true, "Ce candidat n'est pas enregistre pour cette election");
+        elections[_idElec].votes[msg.sender] = _idCand;
+        elections[_idElec].candidats[_idCand].nbreVotes += 1;
+        elections[_idElec].totalVotes += 1;
     }
 
     /** 
      * @dev Récupérer le vote d'un électeur en fournissant l'identifiant de l'élection
-     * @param _election l'identifiant de l'élection
+     * @param _idElec l'identifiant de l'élection
      * @return l'identifiant du candidat voté
      */
-    function getVote(uint _election) public view returns (uint) {
-        return votes[_election][msg.sender];
+    function getVoteElecteur(uint _idElec) electionEnCours(_idElec) public view returns (uint)  {
+        return elections[_idElec].votes[msg.sender];
+    }
+
+    function getNbreVotesCandidat(uint _idElec, uint _idCand) electionEnCours(_idElec) public view returns (uint) {
+        require(elections[_idElec].candidats[_idCand].enregistre == false, "Un candidat avec cet identifiant est deja enregitre pour cette election");
+        return elections[_idElec].candidats[_idCand].nbreVotes;
+    }
+
+    function creerElection(uint _idElec) public {
+        require(elections[_idElec].statut == Statut.Initial, "Cette election n'est pas disponible pour la creation");
+        elections[_idElec].statut = Statut.Cree;
+        elections[_idElec].auteur = msg.sender;
+    }
+
+    function ajouterCandidat(uint _idElec, uint _idCand, string memory _nomCand) auteurSeulement(_idElec) public {
+        require(elections[_idElec].statut == Statut.Cree, "Pas possible d'enregistrer un candidat");
+        require(elections[_idElec].candidats[_idCand].enregistre == false, "Un candidat avec cet identifiant est deja enregitre pour cette election");
+        elections[_idElec].candidats[_idCand].nom = _nomCand;
+        elections[_idElec].candidats[_idCand].enregistre = true;
+        elections[_idElec].candidats[_idCand].nbreVotes = 0;
+    }
+
+    function lancerElection(uint _idElec) auteurSeulement(_idElec) public {
+        require(elections[_idElec].statut == Statut.Cree, "Pas possible de lancer une election qui n'est pas creee");
+        elections[_idElec].statut = Statut.EnCours;
+        elections[_idElec].totalVotes = 0;
+    }
+
+    function terminerElection(uint _idElec) auteurSeulement(_idElec) public {
+        require(elections[_idElec].statut == Statut.EnCours, "Pas possible de terminer une election qui n'est pas en cours");
+        elections[_idElec].statut = Statut.Termine;
     }
 }
--- test/vock.js
+++ test/vock.js
@@ -5,9 +5,151 @@ const Vock = artifacts.require("Vock");
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("Vock", function (/* accounts */) {
-  it("should assert true", async function () {
-    await Vock.deployed();
-    return assert.isTrue(true);
+contract("Vock", async function ([auteur, compte, electeur, autreElecteur]) {
+
+  const _idElec = Math.floor(Math.random() * 100) + 1;
+  const _idCand = 5;
+  const nomCandidat = "Candidat 1";
+
+  function getReason(err) {
+    //console.log(err)
+    return err.data[Object.keys(err.data)[0]].reason;
+  }
+
+  it("Interdire la récupération du nombre de votes si l'élection n'existe pas", async function () {
+    const vock = await Vock.deployed();
+    await vock.getNbreVotesCandidat(_idElec, _idCand).catch((err) => {
+      return assert.isTrue(getReason(err) === "Cette election n'est pas en cours");
+    });
+  });
+
+  it("Ne pas de récupérer de vote si l'élection n'est pas en cours", async function () {
+    const vock = await Vock.deployed();
+    await vock.getVoteElecteur(_idElec).catch((err) => {
+      return assert.isTrue(getReason(err) === "Cette election n'est pas en cours");
+    });
+  });
+
+  it("Ne pas ajouter de candidat si on n'est pas l'auteur de l'élection", async function () {
+    const vock = await Vock.deployed();
+    await vock.ajouterCandidat(_idElec, _idCand, nomCandidat, { from: compte }).catch((err) => {
+      return assert.isTrue(getReason(err) === "Vous n'etes pas l'auteur de cette election");
+    });
+  });
+
+  it("Ne pas lancer une élection dont on est pas l'auteur", async function () {
+    const vock = await Vock.deployed();
+    await vock.lancerElection(_idElec, { from: compte }).catch((err) => {
+      return assert.isTrue(getReason(err) === "Vous n'etes pas l'auteur de cette election");
+    });
+  });
+
+  it("Ne pas voter pour une élection qui n'est pas encore en cours", async function () {
+    const vock = await Vock.deployed();
+    await vock.voter(_idElec, _idCand, { from: electeur }).catch((err) => {
+      return assert.isTrue(getReason(err) === "Cette election n'est pas en cours");
+    });
+  });
+
+  // Création d'une élection
+
+  it("Ne pas voter pendant la phase d'ajout des candidats", async function () {
+    const vock = await Vock.deployed();
+    console.log("        | Création de l'élection ...")
+    await vock.creerElection(_idElec, { from: auteur });
+    await vock.voter(_idElec, _idCand, { from: electeur }).catch((err) => {
+      return assert.isTrue(getReason(err) === "Cette election n'est pas en cours");
+    });
+
+    console.log("        | Ajout d'un candidat ...")
+    await vock.ajouterCandidat(_idElec, _idCand, nomCandidat, { from: auteur });
+  });
+
+  it("Ne pas récupérer un vote pendant la phase d'ajout des candidats", async function () {
+    const vock = await Vock.deployed();
+    await vock.getVoteElecteur(_idElec, { from: electeur }).catch((err) => {
+      return assert.isTrue(getReason(err) === "Cette election n'est pas en cours");
+    });
+  });
+
+  it("Ne pas récupérer le nombre de votes d'un candidat pendant la phase d'ajout des candidats", async function () {
+    const vock = await Vock.deployed();
+    await vock.getNbreVotesCandidat(_idElec, _idCand, { from: compte }).catch((err) => {
+      return assert.isTrue(getReason(err) === "Cette election n'est pas en cours");
+    });
+  });
+
+  it("Ne pas créer une élection déjà créée", async function () {
+    const vock = await Vock.deployed();
+    await vock.creerElection(_idElec, { from: compte }).catch((err) => {
+      return assert.isTrue(getReason(err) === "Cette election n'est pas disponible pour la creation");
+    });
+  });
+
+  // Lancement d'une élection
+
+  it("Ne pas créer une élection déjà lancée", async function () {
+    const vock = await Vock.deployed();
+    console.log("        | Lancement de l'élection ...")
+    await vock.lancerElection(_idElec, { from: auteur });
+
+    await vock.creerElection(_idElec, { from: auteur }).catch((err) => {
+      return assert.isTrue(getReason(err) === "Cette election n'est pas disponible pour la creation");
+    });
+  });
+
+  it("Ne pas relancer une élection déjà lancée", async function () {
+    const vock = await Vock.deployed();
+    await vock.lancerElection(_idElec, { from: auteur }).catch((err) => {
+      return assert.isTrue(getReason(err) === "Pas possible de lancer une election qui n'est pas creee");
+    });
+  });
+
+  it("Ne pas ajouter un candidat après le lancement d'une élection", async function () {
+    const vock = await Vock.deployed();
+    await vock.ajouterCandidat(_idElec, _idCand, nomCandidat, { from: auteur }).catch((err) => {
+      return assert.isTrue(getReason(err) === "Pas possible d'enregistrer un candidat");
+    });
+  });
+
+  it("Ne pas voter pour un candidat non enregistré", async function () {
+    const vock = await Vock.deployed();
+    await vock.voter(_idElec, _idCand + 1, { from: electeur }).catch((err) => {
+      return assert.isTrue(getReason(err) === "Ce candidat n'est pas enregistre pour cette election");
+    });
+  });
+
+  it("Ne pas voter plus d'une fois", async function () {
+    const vock = await Vock.deployed();
+    console.log("        | Voter pour un candidat ...")
+    await vock.voter(_idElec, _idCand, { from: electeur });
+
+    await vock.voter(_idElec, _idCand, { from: electeur }).catch((err) => {
+      return assert.isTrue(getReason(err) === "Ce compte a deja vote");
+    });
+  });
+
+  it("Ne pas terminer une élection dont on n'est pas l'auteur", async function () {
+    const vock = await Vock.deployed();
+    await vock.terminerElection(_idElec, { from: compte }).catch((err) => {
+      return assert.isTrue(getReason(err) === "Vous n'etes pas l'auteur de cette election");
+    });
+  });
+
+  it("Ne pas voter dans une élection déjà terminée", async function () {
+    const vock = await Vock.deployed();
+    console.log("        | Terminer l'élection ...")
+    await vock.terminerElection(_idElec, { from: auteur });
+
+    await vock.voter(_idElec, _idCand, { from: autreElecteur }).catch((err) => {
+      return assert.isTrue(getReason(err) === "Cette election n'est pas en cours");
+    });
+  });
+
+  it("Ne pas relancer une élection déjà terminée", async function () {
+    const vock = await Vock.deployed();
+    await vock.lancerElection(_idElec, { from: auteur }).catch((err) => {
+      return assert.isTrue(getReason(err) === "Pas possible de lancer une election qui n'est pas creee");
+    });
   });
 });
